c76daf6d043443f99ca56b2ec3cdea674285da0f
dessereii <ma.dessereiemaas@gmail.com>
2025-04-03 03:50:50 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+/* ============= typing animation ============== */
+var typed= new Typed(".typing", {
+    strings:["Hello","Kamusta","Hola"],
+    typespeed:20,
+    Backspeed:20,
+    loop:true
+})
+
